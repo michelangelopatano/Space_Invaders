f8ac18239c214ec7dbeef58ee30da0e76d5fd38a
michelangelopatano <michelangelopatano@gmail.com>
2026-05-13 17:33:18 +0200

    Gioco base

--- ship.js
+++ ship.js
@@ -8,7 +8,7 @@ class Ship {
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
-const ship = new Ship(375, 500);
+const ship = new Ship(450, 700);
 
 const bg = new Image();
 const ship_img = new Image();
@@ -31,17 +31,32 @@ ship_img.onload = startIfReady;
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // sfondo
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 
-    // nave
     ctx.drawImage(ship_img, ship.x, ship.y, 50, 50);
 
     requestAnimationFrame(loop);
 }
 
 function right(){
-    if(ship.x<750){
+    if(ship.x<950){
+        ship.x++
+        ship.x++
+        ship.x++
         ship.x++
     }
 }
+
+function left(){
+    if(ship.x>0){
+        ship.x--
+        ship.x--
+        ship.x--
+        ship.x--
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.code === "ArrowLeft") left();
+    if (e.code === "ArrowRight") right();
+});
